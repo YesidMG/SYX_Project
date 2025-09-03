e2b44273c3ebcc48e110128d8ef614c7202627a3
SebeNn <sebasnio17@gmail.com>
2025-09-03 01:31:31 -0500

    Change the order: specific routes first, then parameterized ones; Include captcha in getEntityReport parameters in api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -19,9 +19,19 @@ export async function getEntities(signal) {
 }
 
 // Obtener reportes de entidades
-export async function getEntityReport(signal) {
-  const res = await fetch(`${API_URL}/entities/report`, { signal });
-  if (!res.ok) throw new Error('Error al obtener el reporte de entidades');
+export async function getEntityReport(captcha, signal) {
+  const res = await fetch(`${API_URL}/entities/report`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ captcha }),
+    signal
+  });
+
+  if (!res.ok) {
+    const errorData = await res.json();
+    throw new Error(errorData.error || 'Error al obtener el reporte de entidades');
+  }
+
   return res.json();
 }
 
@@ -30,7 +40,7 @@ export async function postComplaint({ entity_id, title, description, captcha }) 
   const res = await fetch(`${API_URL}/complaints`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ entity_id, title, description, captcha}),
+    body: JSON.stringify({ entity_id, title, description, captcha }),
   });
   if (!res.ok) throw new Error('Error al enviar la queja');
   return res.json();
