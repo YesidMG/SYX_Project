f97c06c6da4710380666eb480550e4563ce2ac01
SebasLaptop <sebasnio17@gmail.com>
2025-09-02 19:34:43 -0500

    hotfix/ prisma import in complaint.service

--- backend/src/application/services/complaint.service.js
+++ backend/src/application/services/complaint.service.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require('../../generated/prisma/client');
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const complaintRepo = require('../../infrastructure/repositories/complaint.repo');
 
