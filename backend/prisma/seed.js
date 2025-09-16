c61493e6b1eeb3b15b9f99b4dd78f8fdad8445b2
SebasLaptop <sebasnio17@gmail.com>
2025-09-16 17:29:21 -0500

    update packege.json for update prisma client

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -41,25 +41,21 @@ async function main() {
     await prisma.complaint.createMany({
         data: [
             {
-                title: 'Demora en trámites',
                 description: 'He experimentado demoras en los trámites administrativos del SENA.',
                 entity_id: createdEntities[4].id, // SENA Regional Boyacá
                 creation_date: new Date(),
             },
             {
-                title: 'Falta de información',
                 description: 'No hay suficiente información sobre los cursos disponibles en la página web.',
                 entity_id: createdEntities[2].id, // Gobernación de Boyacá
                 creation_date: new Date(),
             },
             {
-                title: 'Problemas con la plataforma',
                 description: 'La plataforma del SENA no está funcionando correctamente, lo que dificulta el acceso a los cursos.',
                 entity_id: createdEntities[4].id, // SENA Regional Boyacá
                 creation_date: new Date(),
             },
             {
-                title: 'Atención al cliente deficiente',
                 description: 'El servicio de atención al cliente del ICBF no responde adecuadamente a las consultas.',
                 entity_id: createdEntities[3].id, // ICBF
                 creation_date: new Date(),
