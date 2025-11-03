3e6e944fb5a27d29b589ac9abcafc2126b670459
SebeNn <sebasnio17@gmail.com>
2025-11-03 00:38:33 -0500

    remove and replace PasswordConfirmModal with CheckSessionMiddleware(base)

--- backend/src/routers/complaint.router.js
+++ backend/src/routers/complaint.router.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const complaintController = require('../controllers/complaint.controller')
-const { captchaMiddleware } = require('../middlewares/captcha.middleware')
-const { adminVerifyMiddleware } = require('../middlewares/adminpassword.middleware')
+const { checkSessionMiddleware } = require('../middlewares/check-session.middleware')
 
 router.get('/', complaintController.getAll)
 router.get('/:entityId', complaintController.getAll)
 //router.get('/:id', complaintController.getById)
 router.post('/', complaintController.create)
-router.patch('/:id/state', adminVerifyMiddleware, complaintController.updateState)
+router.patch('/:id/state', checkSessionMiddleware, complaintController.updateState)
 
 // Middleware de manejo de errores
 router.use((err, req, res, next) => {
