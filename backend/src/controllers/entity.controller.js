1e79bb41e29d240ab5260a9c3f67ea18b1e9c059
SebeNn <sebasnio17@gmail.com>
2025-11-07 17:25:14 -0500

    Email sending implementation changed to entities/report endpoint and ready for external service implementation

--- backend/src/controllers/entity.controller.js
+++ backend/src/controllers/entity.controller.js
@@ -30,8 +30,37 @@ exports.create = async (req, res, next) => {
 exports.getReport = async (req, res, next) => {
   try {
     const report = await entityService.getEntitiesWithComplaintCount()
+    if (String(req.query.notify).toLocaleLowerCase() === 'true') {
+      sendEmailNotification(req)
+    }
     res.json(report)
   } catch (err) {
     next(err)
+  }
+}
+
+const sendEmailNotification = async req => {
+  const NotifierApi = require('../apis/notifier.api')
+  const ReportNotificationService = require('../services/report-notification.service')
+
+  const notifierBase = process.env.NOTIFIER_API_URL
+  const notifierKey = process.env.NOTIFIER_API_KEY
+  const defaultRecipient = process.env.NOTIFY_EMAIL_USER
+
+  if (notifierBase && defaultRecipient) {
+    const notifier = new NotifierApi(notifierBase, notifierKey)
+    const reportNotificationService = new ReportNotificationService(notifier, defaultRecipient)
+
+    const recipient = req.query.to || defaultRecipient
+    const userName = req.body.userName
+    const reportName = 'Reporte de Entidades con Conteo de Quejas'
+
+    setImmediate(() => {
+      reportNotificationService.sendNotification({ recipient, userName, reportName }).catch(err => {
+        console.error('Error sending report notification:', err)
+      })
+    })
+  } else {
+    console.warn('Notifier API base URL or default recipient email not configured.')
   }
 }
