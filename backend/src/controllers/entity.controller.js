b0a3ccb9966dc45e4e413cdf8124c0b3a2a2cfe2
Danna <danna.merchan@uptc.edu.co>
2025-11-15 14:34:02 -0500

    feat: add RabbitMQ email event producer

--- backend/src/controllers/entity.controller.js
+++ backend/src/controllers/entity.controller.js
@@ -1,4 +1,5 @@
 const entityService = require('../services/entity.service')
+const sendEmailEvent = require('../rabbitmq/sendEmailEvent')
 
 exports.getAll = async (req, res, next) => {
   try {
@@ -30,37 +31,19 @@ exports.create = async (req, res, next) => {
 exports.getReport = async (req, res, next) => {
   try {
     const report = await entityService.getEntitiesWithComplaintCount()
-    if (String(req.query.notify).toLocaleLowerCase() === 'true') {
-      sendEmailNotification(req)
+
+    // Si viene el query ?notify=true entonces mandamos evento a RabbitMQ
+    if (String(req.query.notify).toLowerCase() === 'true') {
+      await sendEmailEvent({
+        to: req.query.to,
+        userName: req.body.userName,
+        reportName: 'Reporte de Entidades con Conteo de Quejas',
+        generatedAt: new Date(),
+      })
     }
+
     res.json(report)
   } catch (err) {
     next(err)
-  }
-}
-
-const sendEmailNotification = async req => {
-  const NotifierApi = require('../apis/notifier.api')
-  const ReportNotificationService = require('../services/report-notification.service')
-
-  const notifierBase = process.env.NOTIFIER_API_URL
-  const notifierKey = process.env.NOTIFIER_API_KEY
-  const defaultRecipient = process.env.NOTIFY_EMAIL_USER
-
-  if (notifierBase && defaultRecipient) {
-    const notifier = new NotifierApi(notifierBase, notifierKey)
-    const reportNotificationService = new ReportNotificationService(notifier, defaultRecipient)
-
-    const recipient = req.query.to || defaultRecipient
-    const userName = req.body.userName
-    const reportName = 'Reporte de Entidades con Conteo de Quejas'
-
-    setImmediate(() => {
-      reportNotificationService.sendNotification({ recipient, userName, reportName }).catch(err => {
-        console.error('Error sending report notification:', err)
-      })
-    })
-  } else {
-    console.warn('Notifier API base URL or default recipient email not configured.')
   }
 }
