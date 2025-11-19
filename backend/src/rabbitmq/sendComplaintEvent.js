e82854e442d46c7f083b6af85147aa7e509b9fc3
SebeNn <sebasnio17@gmail.com>
2025-11-18 23:48:22 -0500

    Update event payload and Add state change logs

--- backend/src/rabbitmq/sendComplaintEvent.js
+++ backend/src/rabbitmq/sendComplaintEvent.js
@@ -1,18 +1,20 @@
 const amqp = require('amqplib')
 
-async function sendComplaintEvent({ complaintId, prevState, newState }) {
+async function sendComplaintEvent({ complaintId, description, prevState, newState }) {
   const conn = await amqp.connect(process.env.RABBITMQ_URL)
   const channel = await conn.createChannel()
   const queue = 'status-queque'
   await channel.assertQueue(queue, { durable: true })
   const message = JSON.stringify({
     complaintId,
+    description,
     prevState,
     newState,
-    timestamp: new Date().toISOString(),
+    changeDate: new Date().toISOString(),
   })
   channel.sendToQueue(queue, Buffer.from(message))
-  console.log('Evento enviado a RabbitMQ:', message)
+  console.log('[',new Date().toLocaleString(),']: Evento de cambio de estado de queja enviado a RabbitMQ')
+  console.log('Mensaje del evento:', message)
   await channel.close()
   await conn.close()
 }
